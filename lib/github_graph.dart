040457c83bb7b41a3de6923ea796e2313c2f3bde
Matthew Gleich <matthewgleich@gmail.com>
2020-05-09 20:34:29 -0400

    🚧 Update (WIP)

--- lib/github_graph.dart
+++ lib/github_graph.dart
@@ -1,7 +1,24 @@
 library github_graph;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// 📱 Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:github_contributions/github_contributions.dart';
+
+class ContributionGrid extends StatelessWidget {
+  final String githubUserName;
+  final int crossAxisItems;
+
+  ContributionGrid(this.githubUserName, {this.crossAxisItems = 52});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getContributions(githubUserName),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data);
+        }
+      },
+    );
+  }
 }
