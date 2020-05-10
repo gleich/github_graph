7006289f94e970ede2f9dcea328a0b66dfc927bb
Matthew Gleich <matthewgleich@gmail.com>
2020-05-09 23:27:18 -0400

    📄 Add example application

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Container();
   }
 }
